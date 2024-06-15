5b4d7ce3c8560165c2ba936cbac3eb4e6160140a
Feedbaek <nicek789@naver.com>
2024-06-15 17:14:44 +0900

    Init Commit

---
+++ src/main/java/minskim2/JHP_World/JhpWorldApplication.java
@@ -0,0 +1,13 @@
+package minskim2.JHP_World;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhpWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JhpWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/minskim2/JHP_World/JhpWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package minskim2.JHP_World;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JhpWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
