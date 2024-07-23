45ecaf5044e50fdc90e51f2d45d39deac386fe66
Feedbaek <nicek789@naver.com>
2024-07-23 15:09:32 +0900

    Chore: 기본 스키마 수정 및 설정 변경

--- src/main/resources/db/migration/V1.0.0__init.sql
+++ src/main/resources/db/migration/V1.0.0__init.sql
@@ -1,11 +1,11 @@
 # role 테이블 생성
 create table role
 (
-    created_date  datetime(6)  null,
     id            bigint auto_increment
         primary key,
-    modified_date datetime(6)  null,
     name          varchar(255) not null,
+    created_date  datetime(6)  null,
+    modified_date datetime(6)  null,
     constraint role_name_unique
         unique (name)
 );
@@ -13,13 +13,13 @@ create table role
 # member 테이블 생성
 create table member
 (
-    created_date  datetime(6) null,
     id            bigint auto_increment
         primary key,
-    modified_date datetime(6) null,
-    role_id       bigint      not null,
-    name          varchar(30) null,
     oauth2id      varchar(50) not null,
+    name          varchar(30) null,
+    role_id       bigint      not null,
+    created_date  datetime(6) null,
+    modified_date datetime(6) null,
     constraint member_oauth2id_unique
         unique (oauth2id),
     constraint member_role_id_fk
